477d803873338d570e8a85dcc6a3b23251e815aa
Carlyn Maw <carlynorama@gmail.com>
2021-10-14 11:03:42 -0700

    Initial Commit

---
+++ ImageHandlerExample/ImageHandlerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageHandlerExampleApp.swift
+//  ImageHandlerExample
+//
+//  Created by Labtanza on 10/14/21.
+//
+
+import SwiftUI
+
+@main
+struct ImageHandlerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
